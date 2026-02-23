b970651362d49f1c93f78dc058ccff555cea738e
a.23308051280606 <a.23308051280606@cbtis128.edu.mx>
2026-02-23 15:30:49 +0000

    d

---
+++ lib/pagina3.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+// --- PÁGINA 3 ---
+class PaginaTres extends StatelessWidget {
+  const PaginaTres({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tercera Página", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "¡Has llegado al final!",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+              ),
+              onPressed: () => Navigator.popUntil(context, ModalRoute.withName('/')),
+              child: const Text("Volver al Inicio"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
